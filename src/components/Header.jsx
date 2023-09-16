8641d7f859c5efcff4789e3facedccf391fda9b2
Jeronimo Parra <geronimoparra@hotmail.com>
2023-09-16 20:12:53 -0300

    "proyect end 4"

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -17,9 +17,9 @@ export const Header = () => {
             <div className="gap-2 flex m-5 justify-self-end">
                 <h2 className="font-bold text-cyan-600 hidden md:block	">Ir a panel</h2>
                 <div className="md:hidden">  <IconPanel></IconPanel></div>
-             <div>   <IconCampaign></IconCampaign></div>
-              <div >  <IconPower></IconPower></div>
-             
+                <div>   <IconCampaign></IconCampaign></div>
+                <div >  <IconPower></IconPower></div>
+
             </div>
         </div>
     )
